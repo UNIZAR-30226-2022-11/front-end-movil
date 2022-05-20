9700a3ea8b21d83783d2876d3fab44e6a01a1fec
jbuil <jorgebuillanau@gmail.com>
2022-05-20 21:13:14 +0200

    Validación restricciones pantallas login y registro

--- app/src/main/java/eina/unizar/ajedrez/UserSignIn.java
+++ app/src/main/java/eina/unizar/ajedrez/UserSignIn.java
@@ -6,37 +6,33 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.HttpResponse;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import eina.unizar.ajedrez.databinding.UserSignInBinding;
 
 public class UserSignIn extends AppCompatActivity{
 
+    private final String URL = "http://ec2-18-206-137-85.compute-1.amazonaws.com:3000/login";
+
     private UserSignInBinding binding;
+    private String username;
+    private String password;
     private RequestQueue queue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,13 +41,12 @@ public class UserSignIn extends AppCompatActivity{
         setContentView(view);
         queue = Volley.newRequestQueue(this);
         binding.login.setOnClickListener(vista -> {
-            String username = binding.username.getEditText().getText().toString();
-            String password = binding.password.getEditText().getText().toString();
-
+            username = Objects.requireNonNull(binding.username.getEditText()).getText().toString();
+            password = Objects.requireNonNull(binding.password.getEditText()).getText().toString();
             // Verify user account
             // If exists { go to menu } else { displayError }
             try {
-                displayMenu(username,password);
+                tryLogin(username, password);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -59,59 +54,57 @@ public class UserSignIn extends AppCompatActivity{
         binding.register.setOnClickListener(vista -> registerUser());
     }
 
-    private boolean obtenerDatos(String username, String password) throws JSONException {
-        Intent i = new Intent(this, MainPage.class);
-        if(username ==  "" || password == "" || username ==  null || password == null){
-            Log.d("d: ", "Esta vacio" );
-            return false;
+    private boolean validFields() {
+        boolean usernameEmpty = username.isEmpty();
+        boolean passwordEmpty = password.isEmpty();
+        if(usernameEmpty) {
+            binding.username.setError("El nickname no debe ser vacío.");
         }
-       String URL = "http://ec2-18-206-137-85.compute-1.amazonaws.com:3000/login";
+        else {
+            binding.username.setError(null);
+        }
+        if(passwordEmpty) {
+            binding.password.setError("La contraseña no debe ser vacía.");
+        }
+        else {
+            binding.password.setError(null);
+        }
+        return !usernameEmpty && !passwordEmpty;
+    }
+
+    private void login(String username, String password) throws JSONException {
+        Intent i = new Intent(this, MainPage.class);
         JSONObject jsonBody = new JSONObject();
         jsonBody.put("nickname", username);
         jsonBody.put("password", password);
         final String requestBody = jsonBody.toString();
-      StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("res", response);
-                try {
-                    //JSONArray obj1 = new JSONArray((response));
-                   //JSONObject ex =  obj1.getJSONObject(0);
-                   //Log.d("Exito: ", ex.getString("exito")+ "y username" +username);
-                    JSONObject obj = new JSONObject(response);
-                    String nombre = obj.getString("nickname").toString();
-                    String monedas = obj.getString("monedas");
-                    String avatar = obj.getString("avatar");
-                    if( nombre.equals(username)){
-                        i.putExtra("nickname", nombre);
-                        i.putExtra("monedas", monedas);
-                        i.putExtra("avatar", avatar);
-                        startActivity(i);
-                        Log.d("Exito: ", obj.getString("nickname")+ "y username" +username);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, response -> {
+            Log.d("res", response);
+            try {
+                JSONObject obj = new JSONObject(response);
+                String nombre = obj.getString("nickname");
+                String monedas = obj.getString("monedas");
+                String avatar = obj.getString("avatar");
+                if(nombre.equals(username)){
+                    i.putExtra("nickname", nombre);
+                    i.putExtra("monedas", monedas);
+                    i.putExtra("avatar", avatar);
+                    startActivity(i);
                 }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(UserSignIn.this,"Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-                Log.e("onErrorResponse: ", error.getLocalizedMessage() == null ? "" : error.getLocalizedMessage());
-            }
+        }, error -> {
+            Toast.makeText(UserSignIn.this,"Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            Log.e("onErrorResponse", error.getLocalizedMessage() == null ? "" : error.getLocalizedMessage());
         }){
             @Override
-            public byte[] getBody() throws AuthFailureError {
-                try {
-                    return requestBody == null ? null : requestBody.getBytes("utf-8");
-                } catch (UnsupportedEncodingException uee) {
-                    Log.d("d: " ,"Falla aqui");
-                    return null;
-                }
+            public byte[] getBody() {
+                return requestBody.getBytes(StandardCharsets.UTF_8);
             }
-          @Override
-            public Map<String,String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String,String>();
+            @Override
+            public Map<String,String> getHeaders() {
+                Map<String,String> params = new HashMap<>();
                 params.put("content-type","application/json");
                 params.put("Access-Control-Allow-Origin","*");
                 return params;
@@ -119,45 +112,17 @@ public class UserSignIn extends AppCompatActivity{
         };
         stringRequest.setRetryPolicy(new DefaultRetryPolicy( 50000, 5, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         queue.add(stringRequest);
-
-        return false;
     }
 
-    private void displayMenu(String username, String password) throws JSONException {
+    private void tryLogin(String username, String password) throws JSONException {
         queue = Volley.newRequestQueue(this);
-        obtenerDatos(username, password);
-
+        if(validFields()) {
+            login(username, password);
+        }
     }
 
     private void registerUser() {
         Intent i = new Intent(this, UserSignUp.class);
         startActivity(i);
     }
-
-    /*private void validarUsuario(String URL){
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                    if(!response.isEmpty()){
-                        Intent i = new Intent(getApplicationContext(), MainPage.class);
-                        startActivity(i);
-                    }else{
-                        Toast.makeText(UserSignIn.this,"Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-                    }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(UserSignIn.this,error.toString(), Toast.LENGTH_SHORT).show();
-            }
-        }){
-            @Nullable
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String,String>();
-                params.put("user",edtUsuario.get)
-                return super.getParams();
-            }
-        };
-    }*/
 }
--- app/src/main/java/eina/unizar/ajedrez/UserSignUp.java
+++ app/src/main/java/eina/unizar/ajedrez/UserSignUp.java
@@ -4,37 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import eina.unizar.ajedrez.databinding.UserSignUpBinding;
 
 public class UserSignUp extends AppCompatActivity {
 
+    private final String URL = "http://ec2-18-206-137-85.compute-1.amazonaws.com:3000/register";
     private UserSignUpBinding binding;
-    String username;
-    String fullName;
-    String email;
-    String password;
-    String repeatPassword;
-    RequestQueue queue;
+    private String username;
+    private String fullName;
+    private String email;
+    private String password;
+    private String repeatPassword;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,72 +46,129 @@ public class UserSignUp extends AppCompatActivity {
         setContentView(view);
 
         binding.button.setOnClickListener(vista -> {
-            username = binding.username.getEditText().getText().toString();
-            fullName = binding.fullName.getEditText().getText().toString();
-            email = binding.email.getEditText().getText().toString();
-            password = binding.password.getEditText().getText().toString();
-            repeatPassword = binding.repeatPassword.getEditText().getText().toString();
-            Log.d("res", "probado");
-            // Register new user account
-            try {
-                registrarUsuario();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            setResult(RESULT_OK);
-            finish();
-        });
-    }
-
-    private boolean registrarUsuario() throws JSONException {
-        Intent i = new Intent(this, UserSignIn.class);
-      /*  if(password != repeatPassword || username == "" || username == null){ // No coincide la contraseña
-            return false;
-        }*/
-        Log.d("res", "llega "+ username + " " + repeatPassword + " " +password);
-        String URL = "http://ec2-18-206-137-85.compute-1.amazonaws.com:3000/register";
-        JSONObject jsonBody = new JSONObject();
-        jsonBody.put("nickname", username);
-        jsonBody.put("password", password);
-        jsonBody.put("email", email);
-        final String requestBody = jsonBody.toString();
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("res", response);
+            username = Objects.requireNonNull(binding.username.getEditText()).getText().toString();
+            fullName = Objects.requireNonNull(binding.fullName.getEditText()).getText().toString();
+            email    = Objects.requireNonNull(binding.email.getEditText()).getText().toString();
+            password = Objects.requireNonNull(binding.password.getEditText()).getText().toString();
+            repeatPassword = Objects.requireNonNull(binding.repeatPassword.getEditText()).getText().toString();
+            // Validar los campos introducidos por el usuario
+            if(validFields()) {
+                // Registrar el usuario
                 try {
-                   // JSONArray obj1 = new JSONArray((response));
-                    //JSONObject exito =  obj1.getJSONObject(0);
-                   // JSONObject datos =  obj1.getJSONObject(1);
-                  //  Log.d("Exito: ", exito.getString("exito")+ "y username" +username);
-                    JSONObject obj = new JSONObject(response);
-                    boolean bien = obj.getBoolean("exito");
-                    if(bien) {
-                        startActivity(i);
-                        Log.d("res", "Cambio actividad " + bien);
-                    }
+                    registrarUsuario();
+                    Toast.makeText(getApplicationContext(), "Usuario registrado", Toast.LENGTH_SHORT).show();
+                    setResult(RESULT_OK);
+                    finish();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("onErrorResponse: ", error.getLocalizedMessage() == null ? "" : error.getLocalizedMessage());
+        });
+    }
+
+    private boolean validNickname() {
+        if(username.isEmpty()) {
+            binding.username.setError("Introduzca su nickname.");
+            return false;
+        }
+        binding.username.setError(null);
+        return true;
+    }
+
+    private boolean validFullName() {
+        if(fullName.isEmpty()) {
+            binding.fullName.setError("Introduzca su nombre completo.");
+            return false;
+        }
+        binding.fullName.setError(null);
+        return true;
+    }
+
+    private boolean validEmail() {
+        if(email.isEmpty()) {
+            binding.email.setError("Introduzca su correo electrónico.");
+            return false;
+        }
+        else {
+            Pattern pat = Pattern.compile("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$");
+            Matcher mat = pat.matcher(email);
+            if(!mat.matches()) {
+                binding.email.setError("Formato de email inválido. Ej: psoft@unizar.es");
+                return false;
             }
-        }){
-            @Override
-            public byte[] getBody() throws AuthFailureError {
-                try {
-                    return requestBody == null ? null : requestBody.getBytes("utf-8");
-                } catch (UnsupportedEncodingException uee) {
-                    Log.d("d: " ,"Falla aqui");
-                    return null;
+            binding.email.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validPassword() {
+        boolean ok = true;
+        if(password.isEmpty()) {
+            binding.password.setError("Introduzca su contraseña.");
+            ok = false;
+        }
+        else {
+            String regex = "^(?=.*[0-9])"
+                    + "(?=.*[a-z])(?=.*[A-Z])"
+                    + "(?=.*[@#$|%^&+=!?])"
+                    + "(?=\\S+$).{8,20}$";
+            Pattern pat = Pattern.compile(regex);
+            Matcher mat = pat.matcher(password);
+            if(!mat.matches()) {
+                binding.password.setError("La contraseña debe contener entre 8 y 20 caracteres " +
+                        "con al menos una minúscula, una mayúscula, un dígito y " +
+                        "un caracter especial.");
+                ok = false;
+            }
+            else {
+                binding.password.setError(null);
+            }
+        }
+        if(!Objects.equals(password, repeatPassword)) {
+            binding.repeatPassword.setError("Las contraseñas no coinciden.");
+            ok = false;
+        }
+        else {
+            binding.repeatPassword.setError(null);
+        }
+        return ok;
+    }
+
+    private boolean validFields() {
+        Log.d("Valor campos", username + ", " + email + ", " + password + ", " + repeatPassword);
+        boolean ok = validNickname() && validFullName() && validEmail() && validPassword();
+        Log.d("Resultado validación", String.valueOf(ok));
+        return ok;
+    }
+
+    private void registrarUsuario() throws JSONException {
+        Intent i = new Intent(this, UserSignIn.class);
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("nickname", username);
+        jsonBody.put("password", password);
+        jsonBody.put("fullname", fullName);
+        jsonBody.put("email", email);
+        final String requestBody = jsonBody.toString();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, response -> {
+            Log.d("res", response);
+            try {
+                JSONObject obj = new JSONObject(response);
+                boolean exito = obj.getBoolean("exito");
+                Log.d("res", "Resultado registrar usuario = " + exito);
+                if(exito) {
+                    startActivity(i);
                 }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> Log.e("onErrorResponse", error.getLocalizedMessage() == null ? "" : error.getLocalizedMessage())){
+            @Override
+            public byte[] getBody() {
+                return requestBody.getBytes(StandardCharsets.UTF_8);
             }
             @Override
-            public Map<String,String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String,String>();
+            public Map<String,String> getHeaders() {
+                Map<String,String> params = new HashMap<>();
                 params.put("content-type","application/json");
                 params.put("Access-Control-Allow-Origin","*");
                 return params;
@@ -118,6 +176,5 @@ public class UserSignUp extends AppCompatActivity {
         };
         stringRequest.setRetryPolicy(new DefaultRetryPolicy( 50000, 5, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         queue.add(stringRequest);
-        return false;
     }
 }
